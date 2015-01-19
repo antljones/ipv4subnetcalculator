8d6eac11da4fc76c8d4fdb37974e0d8a3434a69a
antljones <antony.l.jones@gmail.com>
2015-01-19 19:29:53 +1300

    Remove redundant code

--- src/ipv4/Address.java
+++ src/ipv4/Address.java
@@ -29,38 +29,29 @@ public class Address {
 	public long toValue() {
 		long intEndIP = 0;
 		
-		if ( ! address.equals( "INVALID" ) ) {	
-			String ipSplit[] = this.getAddress().split( "\\." );
+	    String ipSplit[] = this.getAddress().split( "\\." );
 			
-			int k = 0;
+		int k = 0;
 			
-			do {
-				intEndIP |= Long.parseLong( ipSplit[ k ] );
+		do {
+			intEndIP |= Long.parseLong( ipSplit[ k ] );
 
-				if ( k < 3 ) {
-					intEndIP = intEndIP << 8;
-				}
+			if ( k < 3 ) {
+				intEndIP = intEndIP << 8;
+			}
 				
-				k++;
-			} while( k < 4 );
+			k++;
+		} while( k < 4 );
 			
-			intEndIP &= 0xffffffff;
-			
-		} else {
-			intEndIP = Long.MAX_VALUE;
-		}
+		intEndIP &= 0xffffffff;
 		
 		return intEndIP;
 	}
 	
 	private String toDottedDecimal( long value ) {
-		String host = "";
-			
-		host += ( value >>> 24 ) + "." +
+		return ( value >>> 24 ) + "." +
 				( ( value & 0x0000000000ff0000L ) >>> 16 ) + "." +
 				( ( value & 0x000000000000ff00L ) >>> 8 ) + "." +
 				( value & 0x00000000000000ffL );	
-			
-		return host;
 	}	
 }
